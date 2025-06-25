91dd847001ac387ef33e1a79c2ccab6a8dcd0990
Blaine <blainenicholls@proton.me>
2025-06-25 11:10:06 -0400

    refactor: launch application with GTK_CSD env variable set to zero
    Might change later but I don't see a reason why I need client side
    decorations.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,14 @@ fn main() -> glib::ExitCode {
     dotenvy::dotenv().ok();
     env_logger::init();
     color_eyre::install().ok();
+
+    unsafe {
+        std::env::set_var("GTK_CSD", "0");
+    }
+
     gtk::init().expect("Failed to initialize GTK");
-    gio::resources_register_include!("nett-icon-viewer.gresource").expect("Failed to register resources");
+    gio::resources_register_include!("nett-icon-viewer.gresource")
+        .expect("Failed to register resources");
 
     let app = app::new();
 
